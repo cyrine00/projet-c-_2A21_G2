e53f241d3553a2c16f7b1bbe13b545eceae28868
Nadine <nadine.elleuch@esprit.tn>
2021-11-03 21:32:08 +0100

    base de donnée commune

---
+++ export.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  Fichier créé - mercredi-novembre-03-2021   
+--------------------------------------------------------
+REM INSERTING into SYSTEM.CLIENT
+SET DEFINE OFF;
+Insert into SYSTEM.CLIENT (CIN,NOM,PRENOM,AGE,VILLE,MOBILE,NB_VISITE,CODE_P) values ('12','nn','ll','20','ddd','1452',null,null);
+Insert into SYSTEM.CLIENT (CIN,NOM,PRENOM,AGE,VILLE,MOBILE,NB_VISITE,CODE_P) values ('20','lm','mp','41','bgg','456',null,null);
+Insert into SYSTEM.CLIENT (CIN,NOM,PRENOM,AGE,VILLE,MOBILE,NB_VISITE,CODE_P) values ('5455','lmp','nnn','29','jll','24156',null,null);
+Insert into SYSTEM.CLIENT (CIN,NOM,PRENOM,AGE,VILLE,MOBILE,NB_VISITE,CODE_P) values ('111','nn','lm','4','bh','14',null,null);
+REM INSERTING into SYSTEM.PARKING
+SET DEFINE OFF;
+REM INSERTING into SYSTEM.RESERVATION
+SET DEFINE OFF;
+REM INSERTING into SYSTEM.RESTAURANT
+SET DEFINE OFF;
+REM INSERTING into SYSTEM.TABLE1
+SET DEFINE OFF;
