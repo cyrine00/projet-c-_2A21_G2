9332c09346c9c926c63120292a51e0d0b50e1b5f
cyrine00 <73775729+cyrine00@users.noreply.github.com>
2021-11-12 00:57:35 +0100

    crud final

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,223 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<QPrinter>
+#include<QMessageBox>
+#include<QIntValidator>
+#include<QSqlQueryModel>
+#include<QSqlQuery>
+#include<QtDebug>
+#include<QObject>
+//#include<QDate>
+#include<QTableWidget>
+#include<QPixmap>
+#include<QTextDocument>
+ #include<QDesktopServices>
+#include<QPainter>
+#include "employe.h"
+#include "historique.h"
+#include "QFile"
+#include "QTextStream"
+#include<QPrintDialog>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+
+#include <QSqlQuery>
+
+
+//#include <QTcharts>???????????????????????????????????????????
+//#include <QChartView>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+   ui->l_CIN->setValidator(new QIntValidator(0,999999,this));
+   ui->l_tel->setValidator(new QIntValidator(0,999999999,this));
+   ui->l_salaire->setValidator(new QIntValidator(0,999999999,this));
+
+ui->tab_etud->setModel(E.afficher());
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+
 }
+
+
+void MainWindow::on_ajouter_clicked()
+{
+ ui->tab_etud->setModel(E.afficher());
+
+    int CIN=ui->l_CIN->text().toInt();
+    int salaire=ui->l_salaire->text().toInt();
+    int tel=ui->l_tel->text().toInt();
+
+    QString nom=ui->l_nom->text();
+    QString prenom=ui->l_prenom->text();
+
+    QString sexe(ui->l_sexe->currentText());
+    QString fonction(ui->comboBox->currentText());
+
+Historique h;
+  h.save("prenom:"+ui->l_prenom->text(),"nom :"+ui->l_nom->text(),"numero telephone:"+ui->l_tel->text());
+
+     Employe E(nom,prenom,CIN,fonction,salaire,tel,sexe);
+
+bool test=E.ajouter();
+
+if(test)
+
+    ui->tab_etud->setModel(E.afficher()); //refresh
+        QMessageBox::information(nullptr, QObject::tr("ok"),
+                QObject::tr("Ajout avec succes\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+     ui->tab_etud->setModel(E.afficher()); //refresh
+
+}
+
+
+
+
+void MainWindow::on_button_modifier_clicked()
+{ui->tab_etud->setModel(E.afficher());
+
+    bool    test=E.modifierj(ui->l_CIN->text().toInt(),ui->l_nom->text(),ui->l_prenom->text(),ui->comboBox->currentText(),ui->l_salaire->text().toInt(),ui->l_sexe->currentText(),ui->l_tel->text().toInt());
+
+    if (test)
+          {
+                  ui->tab_etud->setModel(E.afficher()); //refresh
+
+              QMessageBox::information(nullptr,QObject::tr("OK"),
+                                   QObject::tr("modification établie"),
+                                   QMessageBox::Ok);}
+          else{
+          QMessageBox::critical(nullptr,QObject::tr("ERROR404"),
+                                  QObject::tr("modification non établie"),
+                                  QMessageBox::Cancel);}
+}
+
+void MainWindow::on_supprimer_clicked()
+{
+
+    Employe E1; E1.setcin(ui->lineEdit_sup->text().toInt());
+    bool test=E1.supprimer(E1.getcin());
+    QMessageBox msgBox;
+    if(test)
+    {
+         ui->tab_etud->setModel(E1.afficher());
+        msgBox.setText("suppression avec succes.");
+    }
+    else
+          msgBox.setText("echec d'ajout");
+                msgBox.exec();
+
+
+}
+
+void MainWindow::on_radioButton_clicked() //tri par fonction
+{
+    QMessageBox msgBox ;
+        QSqlQueryModel * model= new QSqlQueryModel();
+
+
+
+           model->setQuery("select * from table1 order by fonction");
+
+           model->setHeaderData(0, Qt::Horizontal, QObject::tr("cin"));
+           model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+           model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+           model->setHeaderData(3, Qt::Horizontal, QObject::tr("fonction"));
+           model->setHeaderData(4, Qt::Horizontal, QObject::tr("salaire"));
+           model->setHeaderData(5, Qt::Horizontal, QObject::tr("sexe"));
+   model->setHeaderData(5, Qt::Horizontal, QObject::tr("telephone"));
+                    ui->tab_etud_4->setModel(model);
+                    ui->tab_etud_4->show();
+                    msgBox.setText("Tri avec succés.");
+                    msgBox.exec();
+}
+
+void MainWindow::on_radioButton_3_clicked() //tri par cin
+{
+    QMessageBox msgBox ;
+            QSqlQueryModel * model= new QSqlQueryModel();
+
+
+               model->setQuery("select * from table1 order by CIN ");
+               model->setHeaderData(0, Qt::Horizontal, QObject::tr("cin"));
+               model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+               model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+               model->setHeaderData(3, Qt::Horizontal, QObject::tr("fonction"));
+               model->setHeaderData(4, Qt::Horizontal, QObject::tr("salaire"));
+               model->setHeaderData(5, Qt::Horizontal, QObject::tr("sexe"));
+       model->setHeaderData(5, Qt::Horizontal, QObject::tr("telephone"));
+                        ui->tab_etud_4->setModel(model);
+                        ui->tab_etud_4->show();
+                        msgBox.setText("Tri avec succés.");
+                        msgBox.exec();
+}
+
+void MainWindow::on_radioButton_2_clicked() //tri par nom
+{
+    QMessageBox msgBox ;
+        QSqlQueryModel * model= new QSqlQueryModel();
+
+
+
+           model->setQuery("select * from table1 order by nom");
+
+           model->setHeaderData(0, Qt::Horizontal, QObject::tr("cin"));
+           model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+           model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+           model->setHeaderData(3, Qt::Horizontal, QObject::tr("fonction"));
+           model->setHeaderData(4, Qt::Horizontal, QObject::tr("salaire"));
+           model->setHeaderData(5, Qt::Horizontal, QObject::tr("sexe"));
+   model->setHeaderData(5, Qt::Horizontal, QObject::tr("telephone"));
+                    ui->tab_etud_4->setModel(model);
+                    ui->tab_etud_4->show();
+                    msgBox.setText("Tri avec succés.");
+                    msgBox.exec();
+}
+
+void MainWindow::on_l_r_textChanged(const QString &arg1)
+{
+    Employe a;
+        if(ui->l_r->text() == "")
+            {
+                ui->rech->setModel(a.afficher());
+            }
+            else
+            {
+                 ui->rech->setModel(a.rechercher(ui->l_r->text()));
+            }
+}
+
+
+void MainWindow::on_pushButton_clicked() //PDF
+{
+   Employe emp;
+    emp.genererPDF();
+    QString link="C:/Users/Administrateur/OneDrive/Bureau/liste.pdf";
+        QDesktopServices::openUrl(QUrl(link));
+}
+
+void MainWindow::on_historique_clicked()
+{
+
+QString link="C:/Users/Administrateur/OneDrive/Bureau/Atelier_Connexion/his.txt";
+    QDesktopServices::openUrl(QUrl(link));
+}
+
+
+void MainWindow::on_pushButton_2_clicked() //imprimer
+{
+    QPrinter  printer;
+    printer.setPrinterName("test");
+    QPrintDialog dialog(&printer,this);
+    if (dialog.exec()==QDialog::Rejected) return;
+}
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,22 +1,59 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "employe.h"
 #include <QMainWindow>
-//bonjour
-//heey
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+
+
+private slots:
+    void on_ajouter_clicked();
+
+    void on_supprimer_clicked();
+
+    void on_button_modifier_clicked();
+
+   // void on_pushButton_3_clicked();
+
+  //  void on_tri_clicked();
+
+    void on_historique_clicked();
+
+    //void on_chercher_clicked();
+
+ //   void on_pushButton_2_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_radioButton_3_clicked();
+
+    void on_radioButton_2_clicked();
+
+    //void on_l_r_returnPressed();
+
+    void on_l_r_textChanged(const QString &arg1);
+
+    void on_pushButton_clicked();
+
+
+
+    void on_pushButton_2_clicked();
+
 
 private:
     Ui::MainWindow *ui;
+    Employe E;
 };
+
 #endif // MAINWINDOW_H
