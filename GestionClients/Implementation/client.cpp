eed45d9eb2776b0dc16bced89096bb4c58417970
Nadine <nadine.elleuch@esprit.tn>
2021-11-12 11:03:14 +0100

    Métiers

---
+++ GestionClients/Implementation/client.cpp
@@ -0,0 +1,159 @@
+#include "client.h"
+#include <QSqlQuery>
+#include<QtDebug>
+#include<QObject>
+#include<QSqlQueryModel>
+#include<QString>
+
+
+Client::Client()
+{
+CIN=0; age=0;
+/*nb_visite=0;*/
+mobile=0;
+nom=" "; prenom=" "; /* fidelite=" "*/; ville=" ";
+}
+
+Client::Client(int CIN,int age,int mobile ,QString nom, QString prenom,QString ville)
+{
+     this->CIN=CIN;
+     this->age=age;
+     this->mobile=mobile;
+     this->nom=nom;
+     this->prenom=prenom;
+         this->ville=ville;
+  /* this->fidelite=fidelite;
+      this->nb_visite=nb_visite;*/
+
+
+}
+int Client::getCIN(){return CIN;}
+int Client::getage(){return age;}
+int Client::getmobile(){return mobile;}
+QString Client::getnom(){return nom;}
+QString Client::getprenom(){return prenom;}
+QString Client::getville(){return ville;}
+
+
+void Client::setCIN(int CIN){ this->CIN=CIN;}
+void Client::setage(int age){this->age=age;}
+void Client::setmobile(int mobile){this->mobile=mobile;}
+void Client::setnom(QString nom){  this->nom=nom;}
+void Client::setprenom(QString prenom){this->prenom=prenom;}
+void Client::setville(QString ville){ this->ville=ville;}
+
+
+bool Client::ajouter()
+{
+    QString CIN_string=QString::number(CIN);
+    QString age_string=QString::number(age);
+    QString mobile_string=QString::number(mobile);
+       QSqlQuery query;
+          query.prepare("INSERT INTO CLIENT (CIN, nom, prenom,age,ville,mobile) "
+                        "VALUES (:CIN, :nom, :prenom, :age, :ville, :mobile)");
+          query.bindValue(0, CIN_string);
+          query.bindValue(1, nom);
+          query.bindValue(2, prenom);
+          query.bindValue(3, age_string);
+          query.bindValue(4, ville);
+          query.bindValue(5, mobile_string);
+         return query.exec();
+}
+
+
+
+QSqlQueryModel * Client::afficher()
+{
+QSqlQueryModel *model = new QSqlQueryModel;
+
+model->setQuery("SELECT*  FROM client");
+model->setHeaderData(0, Qt::Horizontal, QObject::tr("CIN"));
+model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+model->setHeaderData(3, Qt::Horizontal, QObject::tr("age"));
+model->setHeaderData(4, Qt::Horizontal, QObject::tr("ville"));
+model->setHeaderData(5, Qt::Horizontal, QObject::tr("mobile"));
+
+return model;
+}
+
+bool Client::modifier()
+{
+    QSqlQuery query;
+       query.prepare("update CLIENT SET NOM=?,PRENOM=?,AGE=?,VILLE=?,MOBILE=? where CIN=?");
+       query.addBindValue(nom);
+       query.addBindValue(prenom);
+       query.addBindValue(age);
+       query.addBindValue(ville);
+       query.addBindValue(mobile);
+       query.addBindValue(CIN);
+      return query.exec();
+
+}
+bool Client::supprimer(int CIN)
+{
+
+    QSqlQuery query;
+       query.prepare("Delete from client where CIN=:CIN ");
+       query.bindValue(0, CIN);
+      return query.exec();
+}
+
+QSqlQueryModel * Client::rechercher(QString c)
+{
+    QString CIN_string=QString::number(CIN);
+    QString age_string=QString::number(age);
+    QString mobile_string=QString::number(mobile);
+     QSqlQueryModel *model=new QSqlQueryModel();
+      model->setQuery("SELECT * FROM client  WHERE CIN LIKE '%"+c+"%'");
+      model->setHeaderData(0, Qt::Horizontal, QObject::tr("CIN"));
+      model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+      model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+      model->setHeaderData(3, Qt::Horizontal, QObject::tr("age"));
+      model->setHeaderData(4, Qt::Horizontal, QObject::tr("ville"));
+      model->setHeaderData(5, Qt::Horizontal, QObject::tr("mobile"));
+
+      return model;
+}
+
+QSqlQueryModel *Client::trie_clnom()
+{
+    QSqlQueryModel *model=new QSqlQueryModel();
+     model->setQuery("SELECT * FROM Client ORDER BY NOM ");
+     model->setHeaderData(0, Qt::Horizontal, QObject::tr("CIN"));
+     model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+     model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+     model->setHeaderData(3, Qt::Horizontal, QObject::tr("age"));
+     model->setHeaderData(4, Qt::Horizontal, QObject::tr("ville"));
+     model->setHeaderData(5, Qt::Horizontal, QObject::tr("mobile"));
+
+     return model;
+}
+
+QSqlQueryModel *Client::trie_clage()
+{
+    QSqlQueryModel *model=new QSqlQueryModel();
+     model->setQuery("SELECT * FROM Client ORDER BY AGE ");
+     model->setHeaderData(0, Qt::Horizontal, QObject::tr("CIN"));
+     model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+     model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+     model->setHeaderData(3, Qt::Horizontal, QObject::tr("age"));
+     model->setHeaderData(4, Qt::Horizontal, QObject::tr("ville"));
+     model->setHeaderData(5, Qt::Horizontal, QObject::tr("mobile"));
+
+     return model;
+}
+
+QSqlQueryModel *Client::trie_clnbvis()
+{
+    QSqlQueryModel *model=new QSqlQueryModel();
+     model->setQuery("SELECT * FROM Client ORDER BY FIDELITE ");
+     model->setHeaderData(0, Qt::Horizontal, QObject::tr("CIN"));
+     model->setHeaderData(1, Qt::Horizontal, QObject::tr("nom"));
+     model->setHeaderData(2, Qt::Horizontal, QObject::tr("prenom"));
+     model->setHeaderData(3, Qt::Horizontal, QObject::tr("age"));
+     model->setHeaderData(4, Qt::Horizontal, QObject::tr("ville"));
+     model->setHeaderData(5, Qt::Horizontal, QObject::tr("mobile"));
+
+     return model;
+}
---
+++ GestionClients/Implementation/client.h
@@ -0,0 +1,42 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include<QString>
+#include <QSqlQuery>
+#include<QSqlQueryModel>
+
+class Client
+{
+public:
+    Client();
+    Client(int,int,int,QString,QString,QString);
+int getCIN();
+int getage();
+int getmobile();
+QString getnom();
+QString getprenom();
+QString getville();
+/*QString getfidelite();*/
+void setCIN(int);
+void setage(int);
+void setmobile(int);
+void setnom(QString);
+void setprenom(QString);
+void setville(QString);
+/*void setnb_visite(int);
+void setfidelite(QString);*/
+
+bool ajouter();
+QSqlQueryModel * afficher();
+bool supprimer(int);
+bool modifier();
+QSqlQueryModel * rechercher(QString c);
+QSqlQueryModel * trie_clnom();
+QSqlQueryModel * trie_clage();
+QSqlQueryModel * trie_clnbvis();
+
+
+private:
+    int CIN,age,mobile,nb_visite,nb_points;
+    QString nom,prenom,ville,fidelite;
+};
+#endif // CLIENT_H
