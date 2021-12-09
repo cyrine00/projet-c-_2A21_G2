e954c072ecf09fe5eb1ebc169ca1f458dcba4f3a
Nadine <nadine.elleuch@esprit.tn>
2021-12-09 15:05:17 +0100

    Integration gestion emp+resto+client

--- connection.cpp
+++ connection.cpp
@@ -8,9 +8,9 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("Projet2A14");
-db.setUserName("azizallani");//inserer nom de l'utilisateur
-db.setPassword("azizallani");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("gestion_cl");
+db.setUserName("system");//inserer nom de l'utilisateur
+db.setPassword("1202");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
